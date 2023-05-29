a713c2a7e4c4cac776123ccf435c516af57f3ab4
Alan OConnor <aocthatsme246@gmail.com>
2023-05-29 18:36:26 +0100

    Finish script
    Finished the script js file and made the html interactive. It now works mostly as expected, it large matches the spec.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,130 @@
+function displayRoundResult (status, playerScore, computerScore) {
+  const container = document.querySelector('.container')
+  const roundInfo = document.querySelector('.round-info')
+  const roundStatus = document.querySelector('.round-status')
+
+  if (status === 'draw') {
+    roundStatus.textContent = 'DRAW'
+    return
+  }
+  container.setAttribute('id', status)
+  roundInfo.setAttribute('id', status)
+  roundStatus.setAttribute('id', status)
+  roundStatus.style.border = 'none'
+  roundStatus.textContent = status === 'win' ? 'WINNER' : 'LOSER'
+
+  setTimeout(() => {
+    clearRoundResult()
+  }, 2000)
+}
+
+function clearRoundResult () {
+  const container = document.querySelector('.container')
+  const roundInfo = document.querySelector('.round-info')
+  const roundStatus = document.querySelector('.round-status')
+
+  container.removeAttribute('id')
+  roundInfo.removeAttribute('id')
+  roundStatus.removeAttribute('id')
+  roundStatus.textContent = 'CHOOSE'
+}
+
+const declareWinner = (playerScore, computerScore) => {
+  let winner;
+  const roundStatus = document.querySelector('.round-status')
+
+  if (playerScore > 4 || computerScore > 4) {
+    playerScore > computerScore ? winner = 'Player' : winner = 'Computer'
+    roundStatus.textContent = `${winner} Wins!`;
+    setTimeout(() => {
+      alert("GAME OVER");
+    }, 3000);
+  }
+}
+
+const buttons = document.querySelectorAll(".buttons");
+
+const getComputerChoice = () => {
+  const computerChoice = Math.floor(Math.random() * 3) + 1
+
+  switch (computerChoice) {
+    case 1:
+      return 'rock'
+    case 2:
+      return 'paper'
+    case 3:
+      return 'scissors'
+  }
+}
+
+const updatePlayerDisplay = (score) => {
+  const display = document.querySelector('#player-score')
+  display.textContent = score
+}
+
+const updateComputerDisplay = (score) => {
+  const display = document.querySelector('#computer-score')
+  display.textContent = score
+}
+
+let playerScore = 0
+updatePlayerDisplay(playerScore)
+let computerScore = 0
+updateComputerDisplay(computerScore)
+
+const playRound = (playerChoice) => {
+  const computerChoice = getComputerChoice()
+
+  if (playerChoice === computerChoice) {
+    displayRoundResult('draw')
+  }
+
+  if (playerChoice === 'rock') {
+    if (computerChoice === 'paper') {
+      displayRoundResult('lose', playerScore, computerScore)
+      computerScore++
+      updateComputerDisplay(computerScore)
+    } else {
+      displayRoundResult("win");
+      playerScore++
+      updatePlayerDisplay(playerScore)
+    }
+  } else if (playerChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      displayRoundResult('lose')
+      computerScore++
+      updateComputerDisplay(computerScore)
+    } else {
+      displayRoundResult("win");
+      playerScore++
+      updatePlayerDisplay(playerScore);
+    }
+  } else {
+    if (computerChoice === 'rock') {
+      displayRoundResult('lose')
+      computerScore++
+      updateComputerDisplay(computerScore);
+    } else {
+      displayRoundResult("win");
+      playerScore++
+      updatePlayerDisplay(playerScore);
+    }
+  }
+
+  declareWinner(playerScore, computerScore);
+}
+
+buttons.forEach((button) => {
+  button.addEventListener('click', button => {
+    setTimeout(() => playRound(button.target.id), 1000)
+  })
+})
+
+/*rockButton.addEventListener('click', (button) => {
+  setTimeout(() => console.log(button.target.id), 2000)
+})*/
+
+/*
 const getComputerChoice = () => Math.floor(Math.random() * 3) + 1
 const getPlayerChoice = () =>
   prompt('Enter Rock, Paper of Scissors').toLowerCase()
@@ -72,3 +199,4 @@ if (playerChoice === 'rock') {
     drawGame(playerChoice)
   }
 }
+*/
